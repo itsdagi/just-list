52a372f3872902e7ee043cdbd6f913d09e425e93
dagimalemux <dagimalemux@gmail.com>
2024-01-17 19:41:40 +0300

    recap

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import './App.css';
 import {useState} from 'react'
 
 export default function App() {
-  const [tasks, setTasks] = useState([{id: 1, name:'dagim'},{id: 2, name:"abebe"},{id: 3, name:"zeleke"},{complited: false}])
+  const [tasks, setTasks] = useState([{id: 1, name:'dagim', completed: false},{id: 2, name:"abebe",completed: false},{id: 3, name:"zeleke",completed: true},])
   return (
     <div className='App'>
-      
+      <h1>
+        Task List
+      </h1>
+         <ul>
+          {tasks.map((task)=>(
+            <li>
+                
+
+            </li>
+          ))}
+         </ul>
     </div>
   );
 }
