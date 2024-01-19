a8d5d45348b49d55ff2bbe6e6c8005604f2f31ef
dagimalemux <dagimalemux@gmail.com>
2024-01-19 18:18:23 +0300

    task list trigger

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import {useState} from 'react'
 
 export default function App() {
   const [tasks, setTasks] = useState([{id: 1, name:'Dagim', completed: false},{id: 2, name:"Abebe",completed: false},{id: 3, name:"Zeleke",completed: true},])
+  const [show, setShow] = useState(true)
+
+  function handleDelete(id){
+    setTasks(tasks.filter(task => id !== task.id))
+  }
+
+  
   return (
     <div className='App'>
       <h1>
         Task List
       </h1>
-
+       <button className='trigger' onClick={() => setShow(!show)}>Toggle</button>
       <ul>
-        {tasks.map((task)=>(
-        <li>
-
+        {show && tasks.map((task)=>(
+        <li  key={task.id} className={task.completed  ? 'completed' : 'incomplete'}>
+       <span>{task.id} - {task.name}</span>  
+         <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
         </li>
         ))}
       </ul>
