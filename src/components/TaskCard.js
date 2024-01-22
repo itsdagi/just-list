d3968214f06e036c3fbfab5bf530b3f2ec4b67d5
dagimalemux <dagimalemux@gmail.com>
2024-01-22 17:04:01 +0300

    handling delete

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -3,7 +3,7 @@ export default function TaskCard(props) {
   return (
     <li  key={props.task.id} className={props.task.completed  ? 'completed' : 'incomplete'}>
      <span>{props.task.id} - {props.task.name}</span>  
-     <button className='delete'>Delete</button>
+     <button onClick={()=> props.handleDelete(props.task.id) } className='delete'>Delete</button>
     </li>
   )
 }
