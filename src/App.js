7257e2e9c61f50ef2e3ac56333255360da8fb1d9
dagimalemux <dagimalemux@gmail.com>
2024-01-23 13:21:34 +0300

    footer starter

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import {useState} from 'react';
 import {Header} from './components/Header';
 import TaskList from './components/TaskList';
-
+import Footer from './components/Footer';
 export default function App() {
 
   return (
     <div className='App'>
       <Header/>
       <TaskList/>
-        
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+      copyright 2024 - BroTask
+    </footer>
+  )
+}
