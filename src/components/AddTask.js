dfb38606c71eca5daf029d263c19dff862f020e5
dagimalemux <dagimalemux@gmail.com>
2024-02-05 15:57:37 +0300

    c

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import "./AddTask.css"
+import {useState} from "react"
 
 export default function AddTask() {
 
-  const [taskValue, setTaskValue] = useState("");
+  const [taskValue, setTaskValue] = useState(" ");
 
   const handleChange =(event)=>{
       setTaskValue(event.target.value)
